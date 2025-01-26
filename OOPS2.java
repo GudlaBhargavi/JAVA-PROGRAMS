6539e8fcdff8a4d1d7b286e99e77d539e4f0ecd5
Bhargavi-student <bhargavi8260@gmail.com>
2025-01-26 19:56:58 +0530

    practised

--- OOPS2.java
+++ OOPS2.java
@@ -7,7 +7,7 @@ public class OOPS2 {
     }
     public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
-      System.out.println("enter two num:");
+      System.out.println("enter two num:2");
       int a=sc.nextInt();
       int b=sc.nextInt();  
 
