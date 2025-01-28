57c475f824c9490efe3832b49c9a5ef559ba2f46
Bhargavi-student <bhargavi8260@gmail.com>
2025-01-28 22:33:24 +0530

    practised

---
+++ MathUtility.java
@@ -0,0 +1,11 @@
+public class MathUtility {
+    
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+       int sum=MathUtility.add(10,9);
+       System.out.println("sum:"+sum); 
+    }
+}
+
